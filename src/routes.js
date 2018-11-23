5f2dc5b034ffbbbb535f0fdcacbd6de296cd5d84
Renan Machado <renan.mav@hotmail.com>
2018-11-23 14:29:33 -0300

    Listagem de prestadores

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -25,10 +26,7 @@ routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.use('/app', authMiddleware)
 
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/logout', SessionController.destroy)
 
